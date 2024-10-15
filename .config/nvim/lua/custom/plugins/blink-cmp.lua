76a1f4606d6837f227023cb28637b7418ff521b0
jacob-waldrip <jacob.waldrip@cityblock.com>
2024-10-15 11:37:06 -0500

    Cyberdream added blink support

--- .config/nvim/lua/custom/plugins/blink-cmp.lua
+++ .config/nvim/lua/custom/plugins/blink-cmp.lua
@@ -6,7 +6,6 @@ return {
   lazy = false, -- lazy loading handled internally
   version = 'v0.*', -- use a release tag to download pre-built binaries
   opts = {
-    highlight = { use_nvim_cmp_as_default = true },
     nerd_font_variant = 'mono',
 
     -- accept = { auto_brackets = { enabled = true } },
