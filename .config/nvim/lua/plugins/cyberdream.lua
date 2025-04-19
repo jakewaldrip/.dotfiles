fafe4dc7ec9221d7cdbb6f477eabf5137c395d79
jacob-waldrip <jacob.waldrip@cityblock.com>
2025-04-19 14:06:12 -0500

    Changed theme around

--- .config/nvim/lua/plugins/cyberdream.lua
+++ .config/nvim/lua/plugins/cyberdream.lua
@@ -1,18 +1,20 @@
 -- Cyberdream, theme setup
 
-return {
-  'scottmckendry/cyberdream.nvim',
-  lazy = false,
-  priority = 1000,
-  config = function()
-    require('cyberdream').setup {
-      transparent = true,
-      italic_comments = true,
-      hide_fillchars = true,
-      borderless_pickers = false,
-      terminal_colors = true,
-      variant = 'default',
-    }
-    vim.cmd 'colorscheme cyberdream'
-  end,
-}
+return {}
+
+-- return {
+--   'scottmckendry/cyberdream.nvim',
+--   lazy = false,
+--   priority = 1000,
+--   config = function()
+--     require('cyberdream').setup {
+--       transparent = false,
+--       italic_comments = true,
+--       hide_fillchars = true,
+--       borderless_pickers = false,
+--       terminal_colors = true,
+--       variant = 'default',
+--     }
+--     vim.cmd 'colorscheme cyberdream'
+--   end,
+-- }
---
+++ .config/nvim/lua/plugins/theme.lua
@@ -0,0 +1,11 @@
+-- return {}
+return {
+  'sainnhe/everforest',
+  lazy = false,
+  priority = 1000,
+  config = function()
+    vim.g.everforest_enable_italic = true
+    vim.g.everforest_background = 'hard'
+    vim.cmd.colorscheme 'everforest'
+  end,
+}
