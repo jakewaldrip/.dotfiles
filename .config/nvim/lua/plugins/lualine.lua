11c69abb9517bd8d019ef30b3b7a543824e91307
Jacob Waldrip <jakeywaldrip@gmail.com>
2025-05-24 18:47:02 -0500

    Updated status line, added rust lsp

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -4,7 +4,7 @@ return {
   opts = {
     options = {
       icons_enabled = true,
-      theme = 'gruvbox_dark',
+      theme = 'everforest',
       component_separators = { left = '', right = '' },
       section_separators = { left = '', right = '' },
       disabled_filetypes = {},
@@ -13,7 +13,6 @@ return {
     sections = {
       lualine_a = { 'mode' },
       lualine_b = {
-        'branch',
         'diff',
         {
           'diagnostics',
@@ -22,19 +21,19 @@ return {
         },
       },
       lualine_c = { 'filename' },
-      lualine_x = { 'copilot', 'encoding', 'fileformat', 'filetype' },
-      lualine_y = { 'progress' },
+      lualine_x = { 'copilot', 'branch' },
+      lualine_y = { 'lsp_status' },
       lualine_z = { 'location' },
     },
     inactive_sections = {
       lualine_a = {},
       lualine_b = {},
-      lualine_c = { 'filename' },
-      lualine_x = { 'location' },
+      lualine_c = {},
+      lualine_x = {},
       lualine_y = {},
       lualine_z = {},
     },
     tabline = {},
-    extensions = {},
+    extensions = { 'lazy' },
   },
 }
