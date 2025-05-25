e7531902c099981d52fee5d0de5bb645b045c2ea
Jacob Waldrip <jakeywaldrip@gmail.com>
2025-05-24 20:02:06 -0500

    Switched rust to use clippy

--- .config/nvim/lsp/rust-analyzer.lua
+++ .config/nvim/lsp/rust-analyzer.lua
@@ -77,20 +77,31 @@ return {
       end
     end)
   end,
+
   capabilities = {
     experimental = {
       serverStatusNotification = true,
     },
   },
+
   before_init = function(init_params, config)
     -- See https://github.com/rust-lang/rust-analyzer/blob/eb5da56d839ae0a9e9f50774fa3eb78eb0964550/docs/dev/lsp-extensions.md?plain=1#L26
     if config.settings and config.settings['rust-analyzer'] then
       init_params.initializationOptions = config.settings['rust-analyzer']
     end
   end,
+
   on_attach = function()
     vim.api.nvim_buf_create_user_command(0, 'LspCargoReload', function()
       reload_workspace(0)
     end, { desc = 'Reload current cargo workspace' })
   end,
+
+  settings = {
+    ["rust_analyzer"] = {
+      check = {
+        command = "clippy"
+      }
+    }
+  }
 }
